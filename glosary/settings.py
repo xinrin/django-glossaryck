4585410c9b3aff7501084c76f71afaa0eb2fa1bd
EmaOlmos <eduardoolmosdionisio@gmai.com>
2023-09-18 20:22:23 -0600

    push

--- glosary/settings.py
+++ glosary/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'storages',
+    'whitenoise.runserver.non_static',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'glosary.urls'
